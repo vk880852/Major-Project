33edf461ac6ee171575d3df40f8025fec01d6a19
Satyendra Kumar Rawat <vk880852@gmail.com>
2024-03-18 23:22:35 +0530

    Add the subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose,{Schema, model} from "mongoose";
+const subscriptionSchema=new Schema(
+    {
+        subcriber:{
+            type:Schema.Types.ObjectId,
+            ref:'User'
+        },
+        channel:
+        {
+            type:Schema.Types.ObjectId,
+            ref:'User'
+        }
+    },
+    {
+        timestamps:true
+    }
+   )
+   export const Subscription=mongoose.model("Subscription",subscriptionSchema);
